0daab600b6b7c53e7f29a71f7352104c31f63ad4
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-02-07 19:34:08 +0000

    add basic project skeleton

---
+++ src/library/service.cpp
@@ -0,0 +1,43 @@
+#include <fractos/service/compute/cuda.hpp>
+
+using namespace fractos::service::compute::cuda;
+
+
+core::future<std::shared_ptr<Service>>
+make_service(std::shared_ptr<core::channel> ch,
+             core::gns::service& gns, const std::string& name,
+             const std::chrono::microseconds& wait_time = std::chrono::seconds{0})
+{
+    CHECK(false);
+}
+
+core::future<std::shared_ptr<Service>>
+make_service(std::shared_ptr<core::channel> ch,
+             core::cap::request& service_req)
+{
+    CHECK(false);
+}
+
+
+Service::~Service()
+{
+    auto& pimpl = impl::Service::get(*this);
+    pimpl.destroy()
+        .get();
+}
+
+core::future<std::shared_ptr<Device>>
+Service::make_device(uint64_t device_id)
+{
+    auto& pimpl = impl::Service::get(*this);
+    CHECK(false);
+}
+
+core::future<void>
+Service::destroy()
+{
+    auto& pimpl = impl::Service::get(*this);
+    if (not pimpl.destroy_sent.test_and_set()) {
+        CHECK(false);
+    }
+}
---
+++ src/library/service_impl.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <fractos/service/compute/cuda.hpp>
+
+
+namespace impl {
+
+    struct Service {
+        Service(Service& other) = delete;
+        Service(const Service& other) = delete;
+
+        static inline impl::Service& get(fractos::service::compute::gpu::Service& obj)
+            { return *reinterpret_cast<impl::Service*>(obj._pimpl.get()); }
+        static inline const impl::Service& get(const fractos::service::compute::gpu::Service& obj)
+            { return *reinterpret_cast<const impl::Service*>(obj._pimpl.get()); }
+        static inline std::shared_ptr<impl::Service> get_ptr(fractos::service::compute::gpu::Service& obj)
+            { return std::static_pointer_cast<impl::Service*>(obj._pimpl); }
+        static inline std::shared_ptr<const impl::Service> get_ptr(const fractos::service::compute::gpu::Service& obj)
+            { return std::static_pointer_cast<const impl::Service*>(obj._pimpl); }
+
+        std::atomic_flag destroy_sent;
+    };
+
+    std::shared_ptr<Service>
+    make_service();
+
+}
