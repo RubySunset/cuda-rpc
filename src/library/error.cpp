9681a18654cd5aef9518ebeb375093a0eaebb27b
5symx <mingxuanyang@yeah.net>
2025-02-11 16:49:48 +0000

    update makefile

---
+++ src/library/error.cpp
@@ -0,0 +1,12 @@
+#include <fractos/service/compute/cuda.hpp>
+
+using namespace fractos::service::compute::cuda;
+
+
+core::future<void>
+handle_error()
+{
+    
+    CHECK(false);
+
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,17 @@
+#include <fractos/common/cmdline.hpp>
+#include <fractos/common/logging.hpp>
+#include <fractos/common/signal.hpp>
+#include <fractos/core/gns.hpp>
+#include <glog/logging.h>
+#include <signal.h>
+
+
+
+using namespace fractos;
+using namespace std::chrono_literals;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
