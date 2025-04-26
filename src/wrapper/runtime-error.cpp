18cf8f857e6d467837d46f87f6c35b48ffefb96d
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-04-26 11:16:38 +0000

    feat(wrapper): add cudaGetErrorName

---
+++ src/wrapper/runtime-error.cpp
@@ -0,0 +1,15 @@
+#include <cuda.h>
+
+#include <./runtime-state.hpp>
+#include <./runtime-syms-extern.hpp>
+
+
+// * error handling
+// https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__ERROR.html
+
+extern "C" [[gnu::visibility("default")]]
+const char* CUDARTAPI
+cudaGetErrorName(cudaError_t error)
+{
+    return (*ptr_cudaGetErrorName)(error);
+}
