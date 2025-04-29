7b70387db0b43534948eb1035d0dda2355cb38d7
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-04-29 02:31:15 +0000

    feat(wrapper): add cudaDriverGetVersion

---
+++ src/wrapper/runtime-version.cpp
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+#include <glog/logging.h>
+
+#include <./runtime-state.hpp>
+#include <./runtime-syms-extern.hpp>
+
+// * version management
+// https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART____VERSION.html
+
+extern "C" [[gnu::visibility("default")]]
+cudaError_t CUDARTAPI
+cudaDriverGetVersion(int *driverVersion)
+{
+    auto& state [[maybe_unused]] = get_runtime_state();
+    auto err = (cudaError_t)cuDriverGetVersion(driverVersion);
+    return_error(err);
+}
