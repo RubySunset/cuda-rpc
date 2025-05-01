eecd983a53f44ff40bd028ae147114611077b41e
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-05-01 19:18:17 +0000

    feat(wrapper): add cudaFree

--- src/wrapper/runtime-memory.cpp
+++ src/wrapper/runtime-memory.cpp
@@ -10,6 +10,20 @@
 
 extern "C" [[gnu::visibility("default")]]
 cudaError_t CUDARTAPI
+cudaFree(void* devPtr)
+{
+    auto& state = get_runtime_state();
+
+    if (devPtr == nullptr) {
+        return cudaSuccess;
+    }
+
+    auto err = (cudaError_t)cuMemFree((CUdeviceptr)devPtr);
+    return_error(err);
+}
+
+extern "C" [[gnu::visibility("default")]]
+cudaError_t CUDARTAPI
 cudaMalloc(void** devPtr, size_t size)
 {
     auto& state = get_runtime_state();
