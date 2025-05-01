881337e88f82c606d90a31ed5726823f8fdd0eef
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-05-01 19:19:26 +0000

    feat(wrapper): add cudaMemcpy

--- src/wrapper/runtime-memory.cpp
+++ src/wrapper/runtime-memory.cpp
@@ -30,3 +30,28 @@ cudaMalloc(void** devPtr, size_t size)
     auto err = (cudaError_t)cuMemAlloc((CUdeviceptr*)devPtr, size);
     return_error(err);
 }
+
+extern "C" [[gnu::visibility("default")]]
+cudaError_t CUDARTAPI
+cudaMemcpy(void *dst, const void *src, size_t count, enum cudaMemcpyKind kind)
+{
+    switch (kind) {
+    case cudaMemcpyHostToHost:
+        return (cudaError_t)cuMemcpy((CUdeviceptr)dst, (CUdeviceptr)src, count);
+        break;
+    case cudaMemcpyHostToDevice:
+        return (cudaError_t)cuMemcpyHtoD((CUdeviceptr)dst, src, count);
+        break;
+    case cudaMemcpyDeviceToHost:
+        return (cudaError_t)cuMemcpyDtoH(dst, (CUdeviceptr)src, count);
+        break;
+    case cudaMemcpyDeviceToDevice:
+        return (cudaError_t)cuMemcpyDtoD((CUdeviceptr)dst, (CUdeviceptr)src, count);
+        break;
+    case cudaMemcpyDefault:
+        return (cudaError_t)cuMemcpy((CUdeviceptr)dst, (CUdeviceptr)src, count);
+        break;
+    default:
+        return cudaErrorInvalidValue;
+    }
+}
