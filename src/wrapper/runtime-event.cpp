59e0841f729eb8d8a010b110c9830b1b8dc587d8
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-05-02 11:05:30 +0000

    feat(wrapper): add cudaEventCreateWithFlags

---
+++ src/wrapper/runtime-event.cpp
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+#include <glog/logging.h>
+
+#include <./runtime-state.hpp>
+#include <./runtime-syms-extern.hpp>
+
+
+// * event management
+// https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__EVENT.html
+
+extern "C" [[gnu::visibility("default")]]
+cudaError_t CUDARTAPI
+cudaEventCreateWithFlags(cudaEvent_t *event, unsigned int flags)
+{
+    auto& state = get_runtime_state();
+
+    auto err = cuEventCreate(event, flags);
+    return_error((cudaError_t)err);
+}
