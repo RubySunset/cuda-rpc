eec51a3a911c625fefecc330307efd369e1e3f81
Lluis Vilanova <vilanova@imperial.ac.uk>
2025-07-29 22:04:15 +0000

    feat(wrapper): add {cu,cuda}OccupancyMaxActiveBlocksPerMultiprocessorWithFlags

---
+++ src/wrapper/driver-occupancy.cpp
@@ -0,0 +1,33 @@
+#include <cuda.h>
+
+#include "./driver-state.hpp"
+#include "./driver-syms-extern.hpp"
+
+
+namespace clt = fractos::service::compute::cuda;
+
+
+// https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__OCCUPANCY.html
+
+extern "C" [[gnu::visibility("default")]]
+CUresult CUDAAPI
+cuOccupancyMaxActiveBlocksPerMultiprocessorWithFlags(int* numBlocks, CUfunction func, int  blockSize, size_t dynamicSMemSize, unsigned int flags)
+{
+    auto& state = get_driver_state();
+
+    auto func_desc = state.get_function(func);
+    if (not func_desc) {
+        return CUDA_ERROR_INVALID_HANDLE;
+    }
+
+    CUresult error = CUDA_SUCCESS;
+    try {
+        *numBlocks = func_desc->function->occupancy_max_active_blocks_per_multiprocessor_with_flags(
+            blockSize, dynamicSMemSize, (CUoccupancy_flags)flags)
+            .get();
+    } catch (const clt::CudaError& e) {
+        error = e.cuerror;
+    }
+
+    return error;
+}
---
+++ src/wrapper/runtime-occupancy.cpp
@@ -0,0 +1,21 @@
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+#include "./runtime-state.hpp"
+
+
+// https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__OCCUPANCY.html
+
+extern "C" [[gnu::visibility("default")]]
+cudaError_t CUDARTAPI
+cudaOccupancyMaxActiveBlocksPerMultiprocessorWithFlags(int* numBlocks, const void* func, int  blockSize, size_t dynamicSMemSize, unsigned int flags)
+{
+    auto& state = get_runtime_state();
+
+    auto [err, cufunc] = state.get_function(func);
+    return_error_maybe(err);
+
+    auto cuerror = (cudaError)cuOccupancyMaxActiveBlocksPerMultiprocessorWithFlags(
+        numBlocks, cufunc, blockSize, dynamicSMemSize, flags);
+    return_error(cuerror);
+}
