9f1e2ba0b2db7792c69748f6ea68fb6cc048d339
Roshan Aekbote <roshan.aekbote20@imperial.ac.uk>
2026-01-09 18:31:27 +0000

    feat: add driver API wrappers for cuda stream memops

---
+++ src/wrapper/driver-memop.cpp
@@ -0,0 +1,53 @@
+#include <cuda.h>
+
+#include <./driver-state.hpp>
+#include <./driver-syms-extern.hpp>
+
+namespace srv = fractos::service::compute::cuda;
+
+
+// https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__MEMOP.html
+
+extern "C" [[gnu::visibility("default")]]
+CUresult
+cuStreamWaitValue32_v2(CUstream stream, CUdeviceptr addr, cuuint32_t value, unsigned int flags)
+{
+    auto& state = get_driver_state();
+
+    auto stream_ptr = state.get_stream(stream);
+
+    if (not stream_ptr) [[unlikely]] {
+        return CUDA_ERROR_INVALID_HANDLE;
+    }
+
+    CUresult error = CUDA_SUCCESS;
+    try {
+        stream_ptr->wait_value_32(addr, value, flags).get();
+    } catch (const srv::CudaError& e) {
+        error = e.cuerror;
+    }
+
+    return error;
+}
+
+extern "C" [[gnu::visibility("default")]]
+CUresult
+cuStreamWriteValue32_v2(CUstream stream, CUdeviceptr addr, cuuint32_t value, unsigned int flags)
+{
+    auto& state = get_driver_state();
+
+    auto stream_ptr = state.get_stream(stream);
+
+    if (not stream_ptr) [[unlikely]] {
+        return CUDA_ERROR_INVALID_HANDLE;
+    }
+
+    CUresult error = CUDA_SUCCESS;
+    try {
+        stream_ptr->write_value_32(addr, value, flags).get();
+    } catch (const srv::CudaError& e) {
+        error = e.cuerror;
+    }
+
+    return error;
+}
