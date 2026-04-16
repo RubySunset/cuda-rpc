64045bb75b82219dc3fb5b291b9a4759e35f0645
Roshan Aekbote <ra2520@ic.ac.uk>
2026-04-16 15:53:12 +0000

    refactor: move driver lib sym extraction to point-of-use

--- src/wrapper/driver-lib.cpp
+++ src/wrapper/driver-lib.cpp
@@ -7,8 +7,36 @@
 
 
 DriverLibSyms& get_driver_lib_syms() {
-    static DriverLibSyms state;
-    return state;
+    static std::shared_ptr<DriverLibSyms> state;
+    static std::mutex mut;
+
+    std::unique_lock lock(mut);
+    if (not state) {
+        state = std::make_shared<DriverLibSyms>();
+        // allow env to override load path
+        auto lib = get_env("FRACTOS_SERVICE_COMPUTE_LIBCUDA", "libcuda.so");
+
+        void* libcuda_handle = dlopen(lib.c_str(), RTLD_LAZY);
+        CHECK(libcuda_handle) << "--reason--> " << dlerror();
+
+        char info[1024];
+        CHECK(dlinfo(libcuda_handle, RTLD_DI_ORIGIN, info) == 0);
+        LOG(INFO) << "opened backend cuda library in "
+                  << info << "/" << lib.substr(lib.find_last_of("/\\") + 1);
+
+        auto do_load_sym = [&](auto name) {
+            LOG(INFO) << "Loading " << name << "...";
+            auto ptr = dlsym(libcuda_handle, name);
+            LOG(INFO) << "ptr == " << ptr;
+            CHECK(ptr) << "--reason--> " << dlerror();
+            return ptr;
+        };
+
+#define SYM(name) state->ptr_ ## name = (decltype(state->ptr_ ## name))do_load_sym(#name);
+#include "./driver-syms.hpp"
+#undef SYM
+    }
+    return *state;
 }
 
 static void reserve_cuda_device_addr_space() __attribute__((constructor));
